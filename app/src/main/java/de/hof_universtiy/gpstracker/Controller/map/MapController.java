7e9261789c71b5b9bf07fbe5e93e5f6febb7debe
apoehlmann <alexander.poehlmann@hof-university.de>
2015-12-09 16:10:04 +0100

    add Map and GPS

---
+++ app/src/main/java/de/hof_universtiy/gpstracker/Controller/map/MapController.java
@@ -0,0 +1,25 @@
+package de.hof_universtiy.gpstracker.Controller.map;
+
+import android.os.Bundle;
+import de.hof_universtiy.gpstracker.Controller.ControllerActivity;
+
+/**
+ * Created by alex on 09.12.15.
+ */
+public class MapController implements ControllerActivity{
+
+    @Override
+    public void onStart(Bundle data) {
+
+    }
+
+    @Override
+    public void onDestroy(Bundle data) {
+
+    }
+
+    @Override
+    public void onPause(Bundle data) {
+
+    }
+}
---
+++ app/src/main/java/de/hof_universtiy/gpstracker/Controller/sensor/GPSController.java
@@ -0,0 +1,21 @@
+package de.hof_universtiy.gpstracker.Controller.sensor;
+
+import android.os.Bundle;
+import de.hof_universtiy.gpstracker.Controller.ControllerActivity;
+import de.hof_universtiy.gpstracker.Controller.ControllerService;
+
+/**
+ * Created by alex on 09.12.15.
+ */
+public class GPSController implements ControllerService {
+
+    @Override
+    public void onStartService(Bundle data) {
+
+    }
+
+    @Override
+    public void onDestroyService(Bundle data) {
+
+    }
+}
