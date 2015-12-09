a4f51cc24c8f98082521ec2c935dfd163e13f82a
apoehlmann <alexander.poehlmann@hof-university.de>
2015-12-09 15:57:37 +0100

    add Interfaces

---
+++ app/src/main/java/de/hof_universtiy/gpstracker/Controller/ControllerActivity.java
@@ -0,0 +1,7 @@
+package de.hof_universtiy.gpstracker.Controller;
+
+/**
+ * Created by alex on 09.12.15.
+ */
+public interface ControllerActivity {
+}
---
+++ app/src/main/java/de/hof_universtiy/gpstracker/Model/Model.java
@@ -0,0 +1,9 @@
+package de.hof_universtiy.gpstracker.Model;
+
+import java.io.Serializable;
+
+/**
+ * Created by alex on 09.12.15.
+ */
+public interface Model extends Serializable{
+}
