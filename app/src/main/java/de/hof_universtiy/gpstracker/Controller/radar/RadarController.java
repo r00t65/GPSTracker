f1b8ee4951ac2fbaa1e768948f96b9bde469c833
alex <alexander.poehlmann@hof-university.de>
2016-01-12 16:38:16 +0100

    add RadarController to RadarFragment

--- app/src/main/java/de/hof_universtiy/gpstracker/Controller/radar/RadarController.java
+++ app/src/main/java/de/hof_universtiy/gpstracker/Controller/radar/RadarController.java
@@ -1,8 +1,10 @@
 package de.hof_universtiy.gpstracker.Controller.radar;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import de.hof_universtiy.gpstracker.Model.position.Location;
 import de.hof_universtiy.gpstracker.Model.radar.Friend;
+import org.osmdroid.views.MapView;
 
 import java.util.List;
 
@@ -11,6 +13,14 @@ import java.util.List;
  * GPSTracker
  */
 public class RadarController implements RadarControllerInterface {
+
+    private final MapView radarView;
+    private final Context context;
+
+    public RadarController(@NonNull Context context,@NonNull MapView lRadarView){
+        this.context = context;
+        this.radarView = lRadarView;
+    }
 
     @Override
     public void setListOfFriends(@NonNull Location myPosition, @NonNull List<Friend> friendList) {
