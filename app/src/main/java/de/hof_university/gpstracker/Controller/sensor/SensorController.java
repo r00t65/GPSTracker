f3dd2570b5b55add962124fe7d2b78b0d66174bd
Gottfried von Recum <gottfried.freiherr.von.recum@hof-university.de>
2016-01-23 13:07:06 +0100

    sensor commit

--- app/src/main/java/de/hof_university/gpstracker/Controller/sensor/SensorController.java
+++ app/src/main/java/de/hof_university/gpstracker/Controller/sensor/SensorController.java
@@ -4,6 +4,9 @@ package de.hof_university.gpstracker.Controller.sensor;
  * Created by alex on 09.12.15.
  */
 public class SensorController implements SensorControllerInterface {
+
+    private boolean isWorking = false;
+
 //TODO:Sensoren integrieren
     @Override
     public void onStartService() {
